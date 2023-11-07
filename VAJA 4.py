7e345929dd0220238bcf3b3c8f34e783b561fa51
Dovecar <149981919+Dovecar@users.noreply.github.com>
2023-11-08 00:38:59 +0100

    Add files via upload

---
+++ VAJA 4.py
@@ -0,0 +1,8 @@
+import as 
+predmeti={"matematika 1" , "IKT", "Od problema do programa" , "informatika", "komuniciranje"}
+poddirektorji= {"vaje, " " predavanja"}
+for predmet in predmeti
+    for direktroji in poddirektorji
+        pot= os.path.join (predmet , direktorji)
+        os.makedirs(pot , exist_ok=TRUE)
+        
