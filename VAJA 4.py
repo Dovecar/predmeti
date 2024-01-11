dd3065ca8a021a23b5d672481232f0aadb46be22
Dovecar <149981919+Dovecar@users.noreply.github.com>
2024-01-11 02:32:16 +0100

    Delete VAJA 4.py

--- VAJA 4.py
+++
@@ -1,8 +0,0 @@
-import as 
-predmeti={"matematika 1" , "IKT", "Od problema do programa" , "informatika", "komuniciranje"}
-poddirektorji= {"vaje, " " predavanja"}
-for predmet in predmeti
-    for direktroji in poddirektorji
-        pot= os.path.join (predmet , direktorji)
-        os.makedirs(pot , exist_ok=TRUE)
-        
